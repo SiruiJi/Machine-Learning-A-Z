d50497212e748f11e45fd2f93195a9612d0dc2ad
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-20 22:42:04 -0400

    Update Multiple_Linear_Regression.py

--- Regression/Multiple_Linear_Regression.py
+++ Regression/Multiple_Linear_Regression.py
@@ -1,1 +1,11 @@
 
+'''In mulitple linear regression, we actually don't need to apply feature scaling, because accroding to the function of the 
+multiple linear regression, we have the coefficients that multipled to each independent variable, therefore, it doesn't matter
+if some feature have higher values than other.'''
+
+'''We don't need check the linear assumption before we apply dataset to linear model. We can just evaluate the model preform.
+if its proform poorly, we just switch to other models.'''
+
+'''We don't need to worry about the select the features that have the highest P-value. Because Multiple Linear Regression model
+will automatically identify the best features, the features that have the highest P values or that are the most statistically 
+significant to figure out how to predict the dependent variable.'''
