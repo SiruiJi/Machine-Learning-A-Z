978bd417dfa9eab5820fea37b3c32b2663577f3e
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-25 10:17:33 -0400

    Update Decision_Tree_Clssification.py

--- Classification/Decision_Tree_Clssification.py
+++ Classification/Decision_Tree_Clssification.py
@@ -10,18 +10,14 @@ y = dataset.iloc[:, -1].values
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-sc_X_train = sc_X.fit_transform(X_train)
-sc_X_test = sc_X.transform(X_test)
 
 from sklearn.tree import DecisionTreeClassifier
 classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
-classifier.fit(sc_X_train, y_train)
+classifier.fit(X_train, y_train)
 
-print(classifier.predict(sc_X.transform([[30,87000]])))
-y_pred = classifier.predict(sc_X_test)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+print(classifier.predict(([[30,87000]])))
+y_pred = classifier.predict(X_test)
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 print(confusion_matrix(y_test, y_pred))
@@ -31,7 +27,7 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
-plt.contourf(X1, X2, classifier.predict(sc_X.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
@@ -47,7 +43,7 @@ from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
-plt.contourf(X1, X2, classifier.predict(sc_X.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('salmon', 'dodgerblue')))
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
