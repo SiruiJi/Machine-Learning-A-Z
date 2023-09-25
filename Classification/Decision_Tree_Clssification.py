6ed9e0527f743206a7e3ceda15546bc3085d7dd0
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-25 16:05:55 -0400

    Update Decision_Tree_Clssification.py

--- Classification/Decision_Tree_Clssification.py
+++ Classification/Decision_Tree_Clssification.py
@@ -23,6 +23,47 @@ from sklearn.metrics import confusion_matrix, accuracy_score
 print(confusion_matrix(y_test, y_pred))
 print(accuracy_score(y_test, y_pred))
 
+from sklearn.metrics import roc_curve, auc
+# Now, let's compute the ROC curve
+probs = classifier.predict_proba(X_test)[:, 1]
+fpr, tpr, thresholds = roc_curve(y_test, probs)
+roc_auc = auc(fpr, tpr)
+
+# Plotting the ROC Curve
+plt.figure(figsize=(10, 8))
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:.2f})')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')  # Random classifier
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic (ROC) Curve')
+plt.legend(loc='lower right')
+plt.grid(True)
+plt.show()
+
+# 1. Random Model
+plt.plot([0, len(y_test)], [0, sum(y_test)], linestyle='--', color='blue', label='Random model')
+
+# 2. Perfect Model
+plt.plot([0, sum(y_test), len(y_test)], [0, sum(y_test), sum(y_test)], color='grey', label='Perfect model')
+
+# 3. Your Model
+probs = classifier.predict_proba(X_test)[:, 1]
+df = pd.DataFrame({'True': y_test, 'Probs': probs})
+df = df.sort_values(by='Probs', ascending=False)
+df['x_values'] = range(0, len(df))
+df['y_cumsum'] = df['True'].cumsum()
+plt.plot(df['x_values'], df['y_cumsum'], color='red', label='Random Forest')
+
+# Plotting details
+plt.xlabel('Total observations')
+plt.ylabel('Class 1 observations')
+plt.title('Cumulative Accuracy Profile (CAP) Curve')
+plt.legend(loc='lower right')
+plt.grid(True)
+plt.show()
+
+
+
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
