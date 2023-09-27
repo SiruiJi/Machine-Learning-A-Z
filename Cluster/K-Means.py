eed6876baf15477a5db470aacd5433378af1a326
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-27 00:33:23 -0400

    Update K-Means.py

--- Cluster/K-Means.py
+++ Cluster/K-Means.py
@@ -1,15 +1,16 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-dataset = pd.read_csv('/')
+dataset = pd.read_csv('/Mall_Customers.csv')
 
 X = dataset.iloc[:, 3:].values
 
 from sklearn.cluster import KMeans
-wscc = []
-for i in range(1: 11):
-    kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
-    kmeans.fit(X)
-    wcss.append(kmeans.inertia_)
+wcss = []
+for i in range(1, 11):
+  kmeans = KMeans(n_clusters = i, init = 'k-means++', random_state = 42)
+  kmeans.fit(X)
+  wcss.append(kmeans.inertia_)
 plt.plot(range(1, 11), wcss)
 plt.title('The Elbow Method')
 plt.xlabel('Number of clusters')
