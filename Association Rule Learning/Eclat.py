076140bf32621f49dc35b40208f270bea55160b8
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-28 23:42:05 -0400

    Update Eclat.py

--- Association Rule Learning/Eclat.py
+++ Association Rule Learning/Eclat.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+dataset = pd.read_csv('/Market_Basket_Optimisation.csv', header = None)
 transactions = []
 for i in range(0, 7501):
   transactions.append([str(dataset.values[i,j]) for j in range(0, 20)])
