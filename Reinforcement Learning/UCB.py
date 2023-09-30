387d00f5417eb51f564f6a9e4ebaa8fed562af6c
SiruiJi <138550153+SiruiJi@users.noreply.github.com>
2023-09-29 21:06:57 -0400

    Update UCB.py

--- Reinforcement Learning/UCB.py
+++ Reinforcement Learning/UCB.py
@@ -6,15 +6,20 @@ dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
 
 import math
 N = 10000
+'''10 thousand round, 10 thousand users'''
 d = 10
+'''10 ads'''
 ads_selected = []
+'''the full list of the ads that are selected over the round'''
 numbers_of_selections = [0] * d
 sums_of_rewards = [0] * d
 total_reward = 0
 for n in range(0, N):
+  ''' we are iterating through are the rounds from first user to last user'''
   ad = 0
   max_upper_bound = 0
   for i in range(0, d):
+  '''loop over different ads one by one'''
     if (numbers_of_selections[i] > 0):
       average_reward = sums_of_rewards[i] / numbers_of_selections[i]
       delta_i = math.sqrt(3/2 * math.log(n + 1) / numbers_of_selections[i])
